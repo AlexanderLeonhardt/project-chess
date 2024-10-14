b9aa233d3f4634fef03a3c513d43b2fc9694e78c
Alex <alexpleonhardt@gmail.com>
2024-10-14 13:22:23 -0400

    socket.io setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-import express from 'express';
-import { createServer } from 'http';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const PORT = 3001;
 
 const app = express();
-const server = createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"]
+  }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log('New connection');
+
+  socket.on('message', (msg) => {
+    console.log('Got message: ', msg);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
